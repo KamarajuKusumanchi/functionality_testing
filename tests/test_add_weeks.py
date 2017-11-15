3a95a56a03d3704d7674f3cd323dd2f82498352a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2017-11-14 20:50:05 -0800

    Add script and functionality tests.

---
+++ tests/test_add_weeks.py
@@ -0,0 +1,39 @@
+
+import unittest
+import pep8
+import os
+import subprocess
+import pandas as pd
+
+# To run the tests
+# cd into the project directory
+# python -m unittest discover tests
+#
+# Sample run:
+# $python -m unittest discover tests
+# .
+# ----------------------------------------------------------------------
+# Ran 1 test in 0.005s
+#
+# OK
+
+
+class functionalTestCase(unittest.TestCase):
+    '''Test if the script is ok functionally or not'''
+
+    def test_functional(self):
+        fname = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'test_defs.json')
+        test_defs = pd.read_json(fname)
+        for row in test_defs.itertuples():
+            print('testing: ', row.cmd)
+            # import time
+            # start = time.time()
+            obtained = subprocess.check_output(row.cmd, universal_newlines=True)
+            # print("time taken = ", time.time()-start)
+            with open(row.out_file, 'r') as fh:
+                expected = fh.read()
+                self.assertEqual(obtained, expected, row.tag)
+
+if __name__ == '__main__':
+    unittest.main()
